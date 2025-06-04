b79f5d153e7cd7d9c6398facc491550099f9a8d6
Dr. Opeyemi Fasanu <94376218+yemyemco@users.noreply.github.com>
2025-06-04 06:48:56 +0100

    Add files via upload

---
+++ Routes/auth.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const authCtrl = require("../Controllers/authCtrl");
+const authToken = require("../middleware/auth");
+const router = express.Router();
+    
+//API for sign up
+router.post("/sign-up", authCtrl.signup);
+
+//API for sign in
+router.post("/sign-in", authCtrl.signin);
+
+//API for creating courses
+router.post("/create-course", authToken.verifyToken, authCtrl.createcourse);
+
+//API for enrolling students
+router.post("/enroll-student", authCtrl.enrollstudent);
+
+//API for view all enrolled students
+router.get("/view-enrolled-students", authCtrl.viewenrolledstudent);
+
+//API for course registration
+router.post("/course-registration", authCtrl.courseregistration);
+
+//API for viewing one enrolled student by course
+router.post("/view-students-by-course", authCtrl.viewstudentsbycourse);
+
+//API to enable students view courses enrolled for
+router.post("/view-course-enrollment", authCtrl.viewcourseenrollment);
+
+//API for viewing course details
+router.get("/get-course-details", authCtrl.getcoursedetails);
+
+module.exports = router;
