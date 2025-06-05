475d1d273c04a3086ac4b45a2c41e8d28e0a93b0
Dr. Opeyemi Fasanu <94376218+yemyemco@users.noreply.github.com>
2025-06-05 16:14:03 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,35 @@
 const jwt = require("jsonwebtoken");
+const { get } = require("mongoose");
 const dotenv = require("dotenv").config();
+const userDB = require("../models/userDB");
 
 //Verify user's token   
 module.exports = 
 {
-    verifyToken: function (req, res, next)
+    verifyToken: async function (req, res, next)
     {
         try {
         let getToken = req.headers["x-access-token"] || req.header("Authorisation") || req.body.token || req.query.token;
-
+        
+        getToken = getToken?.replace("Bearer ", "");
+        
         if(!getToken)
         {
             return res.status(400).json({Message: "Token error: Log in again"});
         }
             
             //Verify token
-            getToken = jwt.verify(getToken, process.env.ACCESS_TOKEN);
+            const decoded = jwt.verify(getToken, process.env.ACCESS_TOKEN);
+
+            const user = await userDB.findOne({token: getToken});
+
+            if(!user)
+            {
+                return res.status(404).json({Message: "User not found"});
+            }
+
+            req.user = user;
+            req.token = getToken;
 
             return next();
 
